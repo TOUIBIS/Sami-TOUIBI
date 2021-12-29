6df1a2747d9129e4caac870103bbf8d708d5fadc
TOUIBIS <touibi.samy@gmail.com>
2021-12-29 13:10:01 +0100

    update ui for Log In screen

---
+++ lib/helper.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+//App Strings
+const String loginTxt = "Log In to Account";
+const String login = "Log In";
+const String loginWithGoogle = "Log In with Google";
+const String signUp = "Sign Up";
+const String or = "Or";
+//App Styles
+const TextStyle normalTextStyle = TextStyle(fontWeight: FontWeight.w500,fontSize: 15,color: Colors.white);
+//App Colors
+const Color bgAppColor = Color.fromRGBO(25, 88, 96, 1);
+const Color unselectedButtonColor = Color.fromRGBO(112, 112, 112, 1);
+const Color loginButtonColor = Color.fromRGBO(19, 65, 71, 1);
--- lib/ui/views/startup/startup_view.dart
+++ lib/ui/views/startup/startup_view.dart
@@ -2,6 +2,7 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:stacked/stacked.dart';
+import 'package:test_technique/helper.dart';
 import 'package:test_technique/ui/views/home/home_view.dart';
 import 'package:test_technique/ui/views/startup/startup_viewmodel.dart';
 
@@ -10,96 +11,147 @@ class StartupView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    TextEditingController nameController = TextEditingController();
+    TextEditingController emailController = TextEditingController();
+    TextEditingController passwordController = TextEditingController();
 
     return ViewModelBuilder<StartupViewModel>.reactive(
          builder: (context,model,child) => Scaffold(
-             body: Container(
-               width: MediaQuery.of(context).size.width,
-               padding: EdgeInsets.only(top: 30,left: 20,right: 20),
-               color: const Color.fromRGBO(25, 88, 96, 1),
-               child: Column(
-                 children: [
-                   Row(
-                     children: [
-                       TextButton(
-                          child: Text('Log In',style: TextStyle(fontWeight: FontWeight.bold,fontSize: 18,color: Colors.white),),
-                           onPressed: (){
+             body: SingleChildScrollView(
+               child: Container(
+                 width: MediaQuery.of(context).size.width,
+                 height: MediaQuery.of(context).size.height,
+                 padding: EdgeInsets.only(top: 30,left: 20,right: 20),
+                 color: bgAppColor,
+                 child: Column(
+                   children: [
+                     Row(
+                       children: [
+                         TextButton(
+                            child: Text(login,style: TextStyle(fontWeight: FontWeight.bold,fontSize: 18,color: Colors.white),),
+                             onPressed: (){
 
-                          }),
-                       SizedBox(
-                         width: 30,
+                            }),
+                         SizedBox(
+                           width: 30,
+                         ),
+                         TextButton(
+                             child: Text(signUp,style: TextStyle(fontWeight: FontWeight.bold,fontSize: 18,color:unselectedButtonColor),),
+                             onPressed: (){
+
+                             })
+                       ],
+                     ),
+                     const Divider(
+                       color: Colors.white,
+                       height: 3,
+                       thickness: 3,
+                       indent: 10,
+                       endIndent: 230,
+                     ),
+                     const SizedBox(
+                       height: 20,
+                     ),
+                     SvgPicture.asset(
+                       'assets/Login.svg',
+                     ),
+                     SizedBox(height: 10,),
+                     Text(loginTxt,style: TextStyle(fontWeight: FontWeight.bold,fontSize: 28,color: Colors.white),),
+                     SizedBox(height: 20,),
+                      TextField(
+                        controller: emailController,
+                        decoration: InputDecoration(
+
+                          border: OutlineInputBorder(
+                            borderSide: BorderSide(color: Colors.white, width: 1.0),
+                          ),
+
+                            enabledBorder: OutlineInputBorder(
+                              borderSide: BorderSide(color: Colors.white, width: 1.0),
+                            ),
+                            focusedBorder: OutlineInputBorder(
+                              borderSide: BorderSide(color: Colors.white, width: 1.0),
+                            ),
+                          labelText: 'EMAIL',
+                          fillColor: Colors.white,
+                          focusColor: Colors.white,
+                          hintText: 'example@nomail.com',
+                          hintStyle: TextStyle(fontWeight: FontWeight.w400,fontSize: 15,color: Colors.white)
+                        )
+                      ),
+                     SizedBox(height: 10,),
+
+                     TextField(
+                         controller: passwordController,
+
+                         decoration: InputDecoration(
+                           suffixIcon: Align(
+                             widthFactor: 1.0,
+                             heightFactor: 1.0,
+                             child: SvgPicture.asset(
+                               'assets/show.svg',
+                               width: 16.1,
+                                 height: 10.15,
+                             ),
+                           ),
+
+                             border: OutlineInputBorder(
+                               borderSide: BorderSide(color: Colors.white, width: 1.0),
+                             ),
+
+                             enabledBorder: OutlineInputBorder(
+                               borderSide: BorderSide(color: Colors.white, width: 1.0),
+                             ),
+                             focusedBorder: OutlineInputBorder(
+                               borderSide: BorderSide(color: Colors.white, width: 1.0),
+                             ),
+                             labelText: 'PASSWORD',
+                             fillColor: Colors.white,
+                             focusColor: Colors.white,
+                             hintStyle: normalTextStyle
+                         )
+                     ),
+                     SizedBox(height: 10,),
+                     Row(
+                       mainAxisAlignment: MainAxisAlignment.end,
+                       children: [
+                         Text('Forgot Password?',style: TextStyle(color: Colors.white,fontWeight: FontWeight.bold),),
+                       ],
+                     ),
+                     SizedBox(height: 10,),
+                     TextButton(
+                       style: TextButton.styleFrom(
+                         backgroundColor: loginButtonColor,
+                         fixedSize: Size(340, 55),
+                         textStyle: const TextStyle(fontSize: 20),
                        ),
-                       TextButton(
-                           child: Text('Sign Up',style: TextStyle(fontWeight: FontWeight.bold,fontSize: 18,color: Color.fromRGBO(112, 112, 112, 1)),),
-                           onPressed: (){
+                       onPressed: () {
 
-                           })
-                     ],
-                   ),
-                   const Divider(
-                     color: Colors.white,
-                     height: 3,
-                     thickness: 3,
-                     indent: 10,
-                     endIndent: 230,
-                   ),
-                   const SizedBox(
-                     height: 20,
-                   ),
-                   SvgPicture.asset(
-                     'assets/Login.svg',
-                     width: 33,
-                     height: 33,
-                   ),
-                   SizedBox(height: 10,),
-                   Text('Log In to Account',style: TextStyle(fontWeight: FontWeight.bold,fontSize: 28,color: Colors.white),),
-                   SizedBox(height: 20,),
-                    TextField(
-                      controller: nameController,
-                      decoration: InputDecoration(
+                       },
+                       child: const Text(login,style: normalTextStyle,),
+                     ),
+                     SizedBox(height: 10,),
+                     TextButton(
+                       style: TextButton.styleFrom(
+                         backgroundColor: loginButtonColor,
+                         fixedSize: Size(340, 55),
+                         textStyle: const TextStyle(fontSize: 20),
+                       ),
+                       onPressed: () {
 
-                        border: OutlineInputBorder(
-                          borderSide: BorderSide(color: Colors.white, width: 1.0),
-                        ),
-
-                          enabledBorder: OutlineInputBorder(
-                            borderSide: BorderSide(color: Colors.white, width: 1.0),
-                          ),
-                          focusedBorder: OutlineInputBorder(
-                            borderSide: BorderSide(color: Colors.white, width: 1.0),
-                          ),
-                        labelText: 'EMAIL',
-                        fillColor: Colors.white,
-                        focusColor: Colors.white,
-                        hintText: 'example@nomail.com',
-                        hintStyle: TextStyle(fontWeight: FontWeight.w400,fontSize: 15,color: Colors.white)
-                      )
-                    ),
-                   SizedBox(height: 10,),
-
-                   TextField(
-                       controller: nameController,
-                       decoration: InputDecoration(
-
-                           border: OutlineInputBorder(
-                             borderSide: BorderSide(color: Colors.white, width: 1.0),
+                       },
+                       child: Row(
+                         mainAxisAlignment: MainAxisAlignment.center,
+                         children: [
+                           SvgPicture.asset(
+                             'assets/google.svg',
                            ),
-
-                           enabledBorder: OutlineInputBorder(
-                             borderSide: BorderSide(color: Colors.white, width: 1.0),
-                           ),
-                           focusedBorder: OutlineInputBorder(
-                             borderSide: BorderSide(color: Colors.white, width: 1.0),
-                           ),
-                           labelText: 'PASSWORD',
-                           fillColor: Colors.white,
-                           focusColor: Colors.white,
-                           hintStyle: TextStyle(fontWeight: FontWeight.w400,fontSize: 15,color: Colors.white)
-                       )
-                   ),
-                   Text('Forgot Password?',textAlign: TextAlign.right,)
-                 ],
+                           SizedBox(width: 10,),
+                           const Text(loginWithGoogle,style: normalTextStyle,),
+                         ],
+                       ),
+                     )
+                   ],
+                 ),
                ),
              )
          ),
@@ -109,17 +161,6 @@ class StartupView extends StatelessWidget {
 }
 
 
-class SignUp extends StatelessWidget {
-  const SignUp({Key? key}) : super(key: key);
 
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      child: Center(
-        child: Text('Sign Up'),
-      ),
-    );
-  }
-}
 
 
